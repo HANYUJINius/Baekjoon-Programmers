26061b205ff4b28d278c00f69321fb470b9a262d
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-11-25 21:57:03 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,6 +1,5 @@
 def solution(age):
     answer = ''
-    num = [i for i in range(0,27)]
     alpha = [chr(word) for word in range(ord('a'), ord('z')+1)]
     for i in str(age):
         answer += alpha[int(i)]
