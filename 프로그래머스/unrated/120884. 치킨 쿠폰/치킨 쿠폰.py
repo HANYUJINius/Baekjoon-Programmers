c492aa08e4ad6c94b0aeed448f97aa170f599323
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-12-02 23:30:45 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,9 @@
+def solution(chicken):
+    answer = 0
+    coupon = chicken
+
+    while coupon >= 10:
+        eat = coupon//10 
+        answer += eat
+        coupon = coupon%10 + eat
+    return answer 
