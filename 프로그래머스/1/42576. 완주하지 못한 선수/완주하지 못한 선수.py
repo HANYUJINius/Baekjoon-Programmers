9a4fb9a196d0dba966e117fed0c7930b36cb30fd
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-12-25 22:58:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.95 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+
+    
+    for i in range(len(participant)-1):
+        if(participant[i] != completion[i]):
+            return participant[i]
+            
+    return participant[-1]
+            
+    
+    
+
+            
